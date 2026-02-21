88cb158478340941704526e5c0c042337b8c28b9
geokuga <za220111239@zapopan.tecmm.edu.mx>
2026-02-21 09:38:21 -0600

    Cambio a HashRouter para GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Cambiamos BrowserRouter por HashRouter
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Menu from "./pages/menu";
 
 function App() {
   return (
+    /* HashRouter no requiere 'basename' para funcionar en subcarpetas de GitHub */
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
+        {/* Opcional: Ruta de captura por si el usuario escribe algo mal */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
