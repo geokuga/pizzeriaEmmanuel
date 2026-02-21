8b457daf2a4d74f1d63d9d5c1f6321fb697673e3
geokuga <za220111239@zapopan.tecmm.edu.mx>
2026-02-21 09:52:59 -0600

    deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from "react";
-// Cambiamos BrowserRouter por HashRouter
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Menu from "./pages/menu";
 
 function App() {
   return (
-    /* HashRouter no requiere 'basename' para funcionar en subcarpetas de GitHub */
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
-        {/* Opcional: Ruta de captura por si el usuario escribe algo mal */}
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
